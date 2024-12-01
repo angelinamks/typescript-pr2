cfa49fc2a82974d74512e02cee6bba1a8d52e7f7
angelinamks <angelinamksmchk@gmail.com>
2024-12-01 00:49:52 -0500

    finish practice 2

---
+++ build/script.js
@@ -0,0 +1,45 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function openModal(content) {
+    const backdrop = document.createElement('div');
+    backdrop.className = 'modal-backdrop';
+    const modal = document.createElement('div');
+    modal.className = 'modal';
+    const closeButton = document.createElement('button');
+    closeButton.className = 'close-button';
+    closeButton.textContent = '✕';
+    closeButton.onclick = () => backdrop.remove();
+    modal.append(closeButton);
+    modal.append(document.createTextNode(content));
+    backdrop.appendChild(modal);
+    document.body.appendChild(backdrop);
+}
+function fetchData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch('https://jsonplaceholder.typicode.com/posts');
+        const posts = yield response.json();
+        const content = posts.slice(0, 5).map(post => post.title).join("\n");
+        openModal(content);
+    });
+}
+document.addEventListener('DOMContentLoaded', () => {
+    const loadDataBtn = document.getElementById('loadDataBtn');
+    loadDataBtn === null || loadDataBtn === void 0 ? void 0 : loadDataBtn.addEventListener('click', () => fetchData());
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+            document.body.style.backgroundColor = '#f0f0f0';
+        }
+        else {
+            document.body.style.backgroundColor = 'white';
+        }
+    });
+});
+//# sourceMappingURL=script.js.map
